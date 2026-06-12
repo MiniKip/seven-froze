7755b8352743b2a55ce2e7998e5529a55822a02f
MiniKip <Minikip@proton.me>
2026-06-12 12:35:59 +0600

    Implement player status and exploration functions
    Added player status display and exploration options.

--- floors/floor_0.lua
+++ floors/floor_0.lua
@@ -1,7 +1,87 @@
--- TABLE
+package.path = "./flintqua/?.lua;./flintqua/?/init.lua;" .. package.path
+local flintqua = require("src.flintqua")
+local tools = require("tools.os_tools")
+local floor_1 = require("floors.floor_1")
+
+local db = flintqua.open("database/playerdata.db")
+
 local floor_0 = {}
 
--- Explore function
+function floor_0.status()
+    local Health = db:get("Player Health")
+    local goldStatus = db:get("Player Gold")
+    
+    tools.clear_screen()
+    for i = 1, 5 do print("") end
+    tools.center_print(" ######################################## ")
+    tools.center_print(" #             SEVEN FROZE              # ")
+    tools.center_print(" #            PLAYER STATUS:            # ")
+    tools.center_print(" #                                      # ")
+    tools.center_print(" #     CURRENT PLAYER HEALTH " .. Health .. "%.      # ")
+    tools.center_print(" #   CURRENT PLAYER INVENTORY " .. goldStatus .. " GOLD.   # ")
+    tools.center_print(" #                                      # ")
+    tools.center_print(" #               1. Back                # ")
+    tools.center_print(" ######################################## ")
+    print("")
+
+    local choice = ""
+    local valid = false
+
+    while not valid do
+        io.write("> ")
+        choice = io.read()
+
+        if choice == "1" then
+            tools.clear_screen()
+            valid = true
+            floor_0.explore()
+        else
+            print("Please choose a correct option.")
+        end
+    end
+end
+
 function floor_0.explore()
-    print(" [             SEVEN FROZE              ] ")
-    print(" [                                      ] ")
+    for i = 1, 5 do print("") end
+    tools.center_print(" ######################################################## ")
+    tools.center_print(" #                      SEVEN FROZE                     # ")
+    tools.center_print(" #                                                      # ")
+    tools.center_print(" #                 You found 2 path ways.               # ")
+    tools.center_print(" #               What do you wanna do now?              # ")
+    tools.center_print(" #                                                      # ") 
+    tools.center_print(" #    1. Go Straight | 2. Go Underground | 3. Status    # ") 
+    tools.center_print(" ######################################################## ")
+    print("")
+
+    local choice = ""
+    local valid = false
+
+    while not valid do
+        io.write("> ")
+        choice = io.read()
+
+        if choice == "1" then 
+            tools.clear_screen()
+            valid = true
+            floor_1.option1(floor_0)
+            
+        elseif choice == "2" then
+            tools.clear_screen()
+            valid = true
+            floor_0.underground()
+            
+        elseif choice == "3" then
+            tools.clear_screen()
+            valid = true
+            floor_0.status()
+        else
+            print("Please choose a correct option.")
+        end
+    end
+end
+
+
+
+
+
+return floor_0
